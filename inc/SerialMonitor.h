75f57b9d55a4f899fa250d8657bc543a75c99c3f
JorgePerC <jorgeperez_1@live.com>
2024-08-07 19:23:47 -0600

    functions to forward output to serial port

---
+++ inc/SerialMonitor.h
@@ -0,0 +1,21 @@
+/*
+ * SerialMonitor.h
+ *
+ *  Created on: Ago 7, 2024
+ *      Author: @JorgePerC
+ */
+
+#ifndef SerialMonitor_H
+#define SerialMonitor_H
+
+#include "stm32f4xx.h"
+#include <stdio.h>
+
+UART_HandleTypeDef huart6;
+DMA_HandleTypeDef hdma_usart1_tx;
+
+
+bool is_USART_enabled();
+void USART_write(int ch);
+
+#endif
---
+++ src/SerialMonitor.c
@@ -0,0 +1,63 @@
+/*
+ * SerialMonitor.cpp
+ *
+ * UART Driver 
+ *  Created on: Apr 12, 2024
+ *      Author: @JorgePerC
+ */
+#ifndef SerialMonitor_C
+#define SerialMonitor_C
+
+#include "SerialMonitor.h"
+// TODO: Set this to a variable when selecting the chip
+
+// Enable clock access
+bool is_USART_enabled(){
+    // TODO: Check that the registers are configured properly
+}bool is_USART_DMA_enabled(){
+    // TODO: Check that the registers are configured properly
+}
+
+// GPIO General Purpose Input/Output
+
+// SPIO Special Purpose Input/Output
+// For this perspective, you active alternate functions
+
+
+void USART_write(int ch) {
+    while (!(USART2 -> SR & 0x0080))
+    {
+        /* code */
+    }
+    USART2->DR = (ch & 0xFF);
+}
+
+// Interface to the C std I/O library
+
+struct __FILE {
+    int handle;
+};
+FILE __stdint = {0};
+FILE __stdout = {1};
+FILE __stderr = {2};
+
+int fgetc(FILE *f){
+    int c;
+
+    c = USART2_read();
+
+    if (c == '\r'){
+        USART_write(c);
+        c = '\n';
+    }
+
+    return c;
+}
+
+
+
+int fputc(int c, FILE *f){
+    return USART2_write(c);
+}
+
+#endif

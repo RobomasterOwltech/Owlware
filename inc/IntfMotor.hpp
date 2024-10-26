35e09231d44207f58d22c4bf81fd3c39f6993e64
sofiaariasv2002 <sofiaariasv2002@gmail.com>
2024-10-26 13:22:05 -0600

    intf switch to class

--- inc/IntfMotor.hpp
+++ inc/IntfMotor.hpp
@@ -12,52 +12,75 @@
 #include "ControllerCAN.hpp"
 #include "ControllerPWM.hpp"
 
-typedef enum { POS, VEL, TOR } OperationModes;
-
-typedef struct {
-    float maxVelocity;
-    float minVelocity;
-    float maxTorque;
-    float minTorque;
-    float maxPosition;
-    float minPosition;
-    float maxTemp;
-    float minTemp;
-} OperationalRanges;
-
 typedef union {
-    ControllerCAN* can;
+     ControllerCAN* can;
     ControllerPWM* pwm;
 } Controller;
-
-//TODO: Create a new union for selecting the ID
-// bc it can be a channel, or for can
-class IntfMotor {
-   protected:
-    Controller contr;
-    OperationalRanges* attr;
-
-    uint8_t mode;
-    uint8_t dir;
-    uint16_t runFreq;
-
-    virtual void actuateVelocity();
-    virtual void actuatePosition();
-    virtual void actuateTorque();
-
-   public:
-    IntfMotor();
-    IntfMotor(ControllerCAN* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction);
-    IntfMotor(ControllerPWM* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction);
-    virtual float getFeedback();
-    void actuate();
-    // The input value is an angular velocity
-    virtual void setReference(float w){};
-    virtual void setControlType(OperationModes mode){};
-    void invert();
-    void stop();
-
-    ~IntfMotor();
+class motor{
+    public:
+        void setReference();
+        void setControlType();
+        float getFeedback();
+        void stop();
+        void invert();
+    private:
+        Controller* comunicationType;
+        int16_t maxVel;
+        int16_t minVel;
+        int8_t Direction;
+        int16_t maxAngle;
+        int16_t minAngle;
+        int16_t maxCurrent;
+        int16_t minCurrent;
+        uint8_t ID;       
 };
+
+// typedef enum { POS, VEL, TOR } OperationModes;
+
+// typedef struct {
+//     float maxVelocity;
+//     float minVelocity;
+//     float maxTorque;
+//     float minTorque;
+//     float maxPosition;
+//     float minPosition;
+//     float maxTemp;
+//     float minTemp;
+// } OperationalRanges;
+
+// typedef union {
+//     ControllerCAN* can;
+//     ControllerPWM* pwm;
+// } Controller;
+
+// //TODO: Create a new union for selecting the ID
+// // bc it can be a channel, or for can
+// class IntfMotor {
+//    protected:
+//     Controller contr;
+//     OperationalRanges* attr;
+
+//     uint8_t mode;
+//     uint8_t dir;
+//     uint16_t runFreq;
+
+//     virtual void actuateVelocity();
+//     virtual void actuatePosition();
+//     virtual void actuateTorque();
+
+//    public:
+//     IntfMotor();
+//     IntfMotor(ControllerCAN* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction);
+//     IntfMotor(ControllerPWM* controller, OperationalRanges* attr, OperationModes mode, uint8_t direction);
+//     virtual float getFeedback();
+//     void actuate();
+//     // The input value is an angular velocity
+//     virtual void setReference(float w){};
+//     virtual void setControlType(OperationModes mode){};
+//     void invert();
+//     void stop();
+
+//     ~IntfMotor();
+// };
 
 #endif /* IntfMotor */

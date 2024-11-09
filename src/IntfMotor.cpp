168671ac5f8d1276e2d7475213a0162962326c89
sofiaariasv2002 <sofiaariasv2002@gmail.com>
2024-11-09 12:08:24 -0600

    change intfMotor.cpp

--- src/IntfMotor.cpp
+++ src/IntfMotor.cpp
@@ -19,22 +19,33 @@ IntfMotor::IntfMotor(Controller* controller, OperationalRanges* attr, OperationM
     this->mode = mode;
 }
 
-virtual float IntfMotor::getFeedback(){
-
-}
-
-void IntfMotor::actuate(int16_t ref){
-
-}
-
-virtual void IntfMotor::setControlType(OperationModes mode){
-
+void IntfMotor::actuate(int16_t ref) {
+    if (mode == VEL) {
+        controller->velocity(ref);
+    } else if (mode == TOR) {
+        controller->torque(ref);
+    } else if (mode == POS) {
+        controller->pos(ref);
+    }
 }
 
 void IntfMotor::invert(uint8_t direction){
-    this -> ref*=-1;
+    this -> direction *= -1;
 }
 
 void IntfMotor::stop(int16_t ref){
     this -> ref=0;
 }
+
+/*
+virtual void actuateVelocity(int16_t ref);
+virtual void actuatePosition(int16_t ref);
+virtual void actuateTorque(int16_t ref);
+
+void setVelocity();
+void setTorque();
+void setPosition();
+
+TODO: implementar funciones???
+
+*/

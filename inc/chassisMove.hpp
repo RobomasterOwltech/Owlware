7ca38f7c1b652b42046748b07ccbd41dd9650b9e
sofiaariasv2002 <sofiaariasv2002@gmail.com>
2025-01-06 14:42:57 -0600

    hacer el movimiento en un metodo, tomar en cuenta el control remoto

--- inc/chassisMove.hpp
+++ inc/chassisMove.hpp
@@ -1,41 +1,36 @@
-/*
- * ControllerCAN.hpp
- *
- *  Created on: April 12, 2024
- *      Author: @JorgePerC
- * 
- * For mor information about how to use this driver: 
- * um2217-description-of-stm32h7-hal-and-lowlayer-drivers-stmicroelectronics-1
- */
+#ifndef CHASSIS_MOVE_HPP
+#define CHASSIS_MOVE_HPP
 
-#ifndef chassisMove_HPP
-#define chassisMove_HPP
+#include "IntfMotor.hpp"
 
-// ===== Includes =====
-#include "IntfMotor.hpp" 
-#include "stm32f4xx_hal.h"
+#define MOTOR_SPEED_TO_CHASSIS_SPEED_WZ
+#define MOTOR_DISTANCE_TO_CENTER
 
 
 class chassisMove {
-    private:
-        IntfMotor* leftFrontMotor;   
-        IntfMotor* rightFrontMotor;  
-        IntfMotor* leftBackMotor;   
-        IntfMotor* rightBackMotor;  
+private:
+    IntfMotor* leftFrontMotor;
+    IntfMotor* rightFrontMotor;
+    IntfMotor* leftBackMotor;
+    IntfMotor* rightBackMotor;
 
-        int16_t speed;  
+    float maxMotorSpeed; 
 
-    public:
-        chassisMove();
-        chassisMove(IntfMotor* leftFrontMotor, IntfMotor* rightFrontMotor,
-                    IntfMotor* leftBackMotor, IntfMotor* rightBackMotor, int16_t speed = 100);
+    float normalizeSpeed(float speed);
 
-        void moveFront();
-        void moveBack();
-        void moveRight();
-        void moveLeft();
+public:
+    // Constructor
+    chassisMove(IntfMotor* leftFrontMotor, IntfMotor* rightFrontMotor,
+                IntfMotor* leftBackMotor, IntfMotor* rightBackMotor, float maxMotorSpeed = 100.0f);
 
-        void stop();
+    // Traducir joystick a movimiento de motores
+    void joystickToMotors(float x, float y, float w);
+
+    // Método para actualizar las velocidades basándose en el joystick
+    void update();
+
+    //Metodo para detener motores 
+    void stop();
 };
 
-#endif /*chassisMove_HPP*/
+#endif /* CHASSIS_MOVE_HPP */
--- src/chassisMove.cpp
+++ src/chassisMove.cpp
@@ -1,29 +1,46 @@
 #include "chassisMove.hpp"
 
+// Constructor
 chassisMove::chassisMove(IntfMotor* leftFrontMotor, IntfMotor* rightFrontMotor,
-                    IntfMotor* leftBackMotor, IntfMotor* rightBackMotor, int16_t speed = 100);
+                         IntfMotor* leftBackMotor, IntfMotor* rightBackMotor, float maxMotorSpeed)
+    : leftFrontMotor(leftFrontMotor), rightFrontMotor(rightFrontMotor),
+      leftBackMotor(leftBackMotor), rightBackMotor(rightBackMotor), maxMotorSpeed(maxMotorSpeed) {}
 
-void chassisMove::moveFront() {
-
+// Normalizar la velocidad para que esté dentro del rango permitido
+float chassisMove::normalizeSpeed(float speed) {
+    if (speed > maxMotorSpeed) return maxMotorSpeed;
+    if (speed < -maxMotorSpeed) return -maxMotorSpeed;
+    return speed;
 }
 
+// Traducir joystick a velocidades de las ruedas
+void chassisMove::joystickToMotors(float x, float y, float w) {
+    float wheel_speed[4];
 
-void chassisMove::moveBack() {
+    // Calcular velocidades de las ruedas mecanum
+    wheel_speed[0] = -x - y + w; // Delantera izquierda
+    wheel_speed[1] = x - y + w;  // Delantera derecha
+    wheel_speed[2] = x + y + w;  // Trasera derecha
+    wheel_speed[3] = -x + y + w; // Trasera izquierda
 
+    // Normalizar y actuar las velocidades en los motores
+    leftFrontMotor->actuate(normalizeSpeed(wheel_speed[0]));
+    rightFrontMotor->actuate(normalizeSpeed(wheel_speed[1]));
+    rightBackMotor->actuate(normalizeSpeed(wheel_speed[2]));
+    leftBackMotor->actuate(normalizeSpeed(wheel_speed[3]));
 }
 
-void chassisMove::moveRight() {
-    leftFrontMotor->actuate(speed);   
-    rightFrontMotor->actuate(-speed); 
-    leftBackMotor->actuate(-speed);
-    rightBackMotor->actuate(speed);
+void getJoystickRotation(){
+    w = (-wheel_speed[0] - wheel_speed[1] - wheel_speed[2] - wheel_speed[3]) * MOTOR_SPEED_TO_CHASSIS_SPEED_WZ / MOTOR_DISTANCE_TO_CENTER;
 }
 
-void chassisMove::moveLeft() {
-    leftMotor->actuate(-speed);  
-    rightMotor->actuate(speed); 
-    leftBackMotor->actuate(speed); 
-    rightBackMotor->actuate(-speed);
+// Actualizar el movimiento basado en joystick
+void chassisMove::update() {
+    float x = getJoystickX();
+    float y = getJoystickY();
+    float w = getJoystickRotation();
+
+    joystickToMotors(x, y, w);
 }
 
 void chassisMove::stop() {

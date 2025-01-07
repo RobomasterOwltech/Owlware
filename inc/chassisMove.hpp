7c9b265cf9e4bfe71694c4da52ec8343cdf8313c
sofiaariasv2002 <sofiaariasv2002@gmail.com>
2025-01-07 15:55:42 -0600

    Obtener torción por medio de xy del joy2 y documentar

--- inc/chassisMove.hpp
+++ inc/chassisMove.hpp
@@ -1,3 +1,16 @@
+ /*
+ * chassisMove.hpp
+ *
+ *  Created on: Jan 02, 2025
+ *      Author: @sofiaariasv2002
+ *              @AnaValeria
+ * 
+ * For mor information:
+ * https://learning.oreilly.com/library/view/wheeled-mobile-robotics/9780128042380/B9780128042045000020_1.xhtml#s0070
+ * https://www.robomaster.com/en-US/products/components/general/M3508
+ *
+ */
+
 #ifndef CHASSIS_MOVE_HPP
 #define CHASSIS_MOVE_HPP
 
@@ -6,7 +19,16 @@
 
 #define CHASSIS_RADIUS 0.3f  // Radio del chasis (distancia del centro a una rueda) en metros
 #define MAX_MOTOR_SPEED 1.0f // Velocidad máxima del motor
+#define K_TWIST 1.0f         // Sensibilidad para torsión del chasis
+#define PI 3.14159265358979323846
 
+/**
+ * @brief Clase para controlar el movimiento de un chasis mecanum utilizando joysticks.
+ * 
+ * Esta clase permite controlar el movimiento de un robot con chasis mecanum. La clase proporciona 
+ * métodos para convertir las entradas de joystick en velocidades de motor, además de normalizar 
+ * las velocidades y calcular la torsión en base a las entradas de control.
+ */
 class chassisMove {
 private:
     IntfMotor* leftFrontMotor;
@@ -17,13 +39,14 @@ private:
     float maxMotorSpeed; 
 
     float normalizeSpeed(float speed);
+    float normalizeW(float w);
 
 public:
     chassisMove(IntfMotor* leftFrontMotor, IntfMotor* rightFrontMotor,
                 IntfMotor* leftBackMotor, IntfMotor* rightBackMotor, 
                 float maxMotorSpeed = MAX_MOTOR_SPEED);
 
-    void joystickToMotors(float x, float y, float w);
+    void joystickToMotors(float x1, float y1, float x2, float y2, float theta_robot);
 
     void stop();
 };
--- src/chassisMove.cpp
+++ src/chassisMove.cpp
@@ -1,9 +1,30 @@
 #include "chassisMove.hpp"
 
+/**
+ * @brief Normaliza la velocidad del motor para asegurarse de que no exceda la velocidad máxima.
+ * 
+ * @param speed Velocidad a normalizar.
+ * @return La velocidad normalizada.
+*/
 float chassisMove::normalizeSpeed(float speed) {
     if (speed > maxMotorSpeed) return maxMotorSpeed;
     if (speed < -maxMotorSpeed) return -maxMotorSpeed;
     return speed;
+}
+
+/**
+ * @brief Normaliza la velocidad angular (torsión) en función de los valores de entrada del joystick.
+ * 
+ * @param theta_joy Ángulo del joystick que indica la dirección de la torsión.
+ * @param theta_robot Ángulo actual del robot.
+ * @return La velocidad angular normalizada en radianes por segundo.
+ */
+float chassisMove::normalizeW(float theta_joy_rads, float theta_robot_rads) {
+    float angle_error = theta_joy_rads - theta_robot_rads;
+    if (angle_error > M_PI) angle_error -= 2 * PI;
+    if (angle_error < -M_PI) angle_error += 2 * PI;
+    float w_rs = K_TWIST * angle_error;
+    return w_rs;
 }
 
 chassisMove::chassisMove(IntfMotor* leftFrontMotor, IntfMotor* rightFrontMotor,
@@ -13,12 +34,27 @@ chassisMove::chassisMove(IntfMotor* leftFrontMotor, IntfMotor* rightFrontMotor,
       leftBackMotor(leftBackMotor), rightBackMotor(rightBackMotor),
       maxMotorSpeed(maxMotorSpeed) {}
 
+/**
+ * @brief Convierte las entradas de los joysticks en velocidades de los motores.
+ * 
+ * Este método toma las entradas de los dos joysticks (para movimiento y torsión) y las convierte en 
+ * velocidades para cada rueda del chasis mecanum.
+ * 
+ * @param x1 Entrada del joystick 1 (eje X para desplazamiento en el plano horizontal).
+ * @param y1 Entrada del joystick 1 (eje Y para desplazamiento en el plano vertical).
+ * @param x2 Entrada del joystick 2 (eje X para control de torsión).
+ * @param y2 Entrada del joystick 2 (eje Y para control de torsión).
+ * @param theta_robot Ángulo de orientación del robot (en radianes).
+ * @param theta_joy Ángulo de orientación del joystick2 (en radianes).
+ */
+void chassisMove::joystickToMotors(float x1, float y1, float x2, float y2, float theta_robot_rads) {
+    // Cálculo del ángulo deseado 
+    float theta_joy_rads = atan2(y2, x2);
+    // Cálculo de la torsión (velocidad angular)
+    float w_rs = normalizeW(theta_joy_rads, theta_robot_rads)
 
-void chassisMove::joystickToMotors(float x, float y, float w) {
-    //v=M*u
     // u
-    Eigen::Vector3f joystick_input(x, y, w);
-
+    Eigen::Vector3f joystick_input(x1, y1, w);
     // M
     Eigen::MatrixXf control_matrix(4, 3);
     control_matrix << -1, -1,  CHASSIS_RADIUS,  // Delantera izquierda
@@ -26,14 +62,14 @@ void chassisMove::joystickToMotors(float x, float y, float w) {
                        1,  1,  CHASSIS_RADIUS,  // Trasera derecha
                       -1,  1,  CHASSIS_RADIUS;  // Trasera izquierda
 
-    // v
+    //v=M*u
     Eigen::VectorXf wheel_speed = control_matrix * joystick_input;
     wheel_speed = wheel_speed.unaryExpr([this](float speed) { return normalizeSpeed(speed); });
 
-    leftFrontMotor->actuate(wheel_speed[0]);
-    rightFrontMotor->actuate(wheel_speed[1]);
-    rightBackMotor->actuate(wheel_speed[2]);
-    leftBackMotor->actuate(wheel_speed[3]);
+    leftFrontMotor->actuate(wheel_speed[0]);   // Delantera izquierda
+    rightFrontMotor->actuate(wheel_speed[1]);  // Delantera derecha
+    rightBackMotor->actuate(wheel_speed[2]);   // Trasera derecha
+    leftBackMotor->actuate(wheel_speed[3]);    // Trasera izquierda
 }
 
 void chassisMove::stop() {
